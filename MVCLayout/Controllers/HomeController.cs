fa0288664125f233ab55fb0118b2f4f615e42fb9
= <=>
2020-07-30 17:00:23 +0700

    Add project files.

---
+++ MVCLayout/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Greeting = "Hello World";
+            ViewBag.Product = new ProductModel()
+            {
+                Id = 1,
+                Name = "Đăng Khoa",
+                Brand = "Đinh",
+                Price = 100000
+            };
+            return View();
+        }
+    }
+}
---
+++ PassingData/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassingData.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public Customer()
+        {
+            CustomerID = 1;
+            Name = "Dang Khoa";
+            Address = "5/29a, Thoi An A, My Thanh, Long Xuyen, An Giang";
+        }
+    }
+}
